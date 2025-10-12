4879812009c33ca08d932fe87588ba25ab3a50bc
Ansh24-09 <240212371@geu.ac.in>
2025-10-12 14:41:19 +0530

    Update parking.h

--- parking.h
+++ parking.h
@@ -1,9 +1,11 @@
 #ifndef PARKING_H
 #define PARKING_H
 
+#define MAX_SLOTS 10
+
 struct Slot {
     int id;
-    int hours;     
+    int hours;
     struct Slot *next;
 };
 
@@ -18,5 +20,6 @@ struct Park {
 struct Slot* addSlot(struct Slot *head, int id, int hours);
 void showSlots(struct Slot *head);
 void saveSlots(struct Slot *head);
+int getNextFreeSlot(struct Slot *head);
 
 #endif
