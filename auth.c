b3e54fffb7333877ffb553df0daf61cefd0bd34e
Ansh24-09 <240212371@geu.ac.in>
2025-10-12 13:43:05 +0530

    auth.c

--- auth.c
+++ auth.c
@@ -1,44 +1,27 @@
-#include <stdio.h>
-#include <string.h>
-#include "auth.h"
+#include "parking.h"
 
-int regUser() {
+int login() {
     User u;
-    FILE *fp = fopen("users.txt", "a");
-    if (!fp) return 0;
-
-    printf("Enter username: ");
-    scanf("%s", u.user);
-    printf("Enter password: ");
-    scanf("%s", u.pass);
-
-    fprintf(fp, "%s %s\n", u.user, u.pass);
-    fclose(fp);
-    printf("User Registered!\n");
-    return 1;
-}
-
-int loginUser() {
-    User u, t;
-    FILE *fp = fopen("users.txt", "r");
-    if (!fp) {
-        printf("No users found. Please register first.\n");
+    char n[20], p[20];
+    FILE *f = fopen("users.txt", "r");
+    if (!f) {
+        printf("Error: users.txt not found!\n");
         return 0;
     }
 
-    printf("Enter username: ");
-    scanf("%s", t.user);
-    printf("Enter password: ");
-    scanf("%s", t.pass);
+    printf("\nUsername: ");
+    scanf("%s", n);
+    printf("Password: ");
+    scanf("%s", p);
 
-    while (fscanf(fp, "%s %s", u.user, u.pass) != EOF) {
-        if (strcmp(u.user, t.user) == 0 && strcmp(u.pass, t.pass) == 0) {
-            fclose(fp);
-            printf("Login Success!\n");
+    while (fscanf(f, "%s %s", u.name, u.pass) != EOF) {
+        if (strcmp(n, u.name) == 0 && strcmp(p, u.pass) == 0) {
+            fclose(f);
             return 1;
         }
     }
-    fclose(fp);
-    printf("Invalid credentials.\n");
+
+    fclose(f);
+    printf("Invalid credentials!\n");
     return 0;
 }
