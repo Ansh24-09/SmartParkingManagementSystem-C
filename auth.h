d43011069e157567ed59e6e22cb0048c2f8906cf
Ansh24-09 <240212371@geu.ac.in>
2025-10-12 14:18:49 +0530

    Update auth.h

--- auth.h
+++ auth.h
@@ -1,12 +1,8 @@
 #ifndef AUTH_H
 #define AUTH_H
 
-typedef struct {
-    char user[30];
-    char pass[30];
-} User;
-
-int regUser();
-int loginUser();
+int login();
+void registerUser();
 
 #endif
+
