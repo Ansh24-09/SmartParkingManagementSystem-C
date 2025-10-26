d64d813fe9d38a311ec4a444bc8fca09c3daf7ee
Ansh24-09 <240212371@geu.ac.in>
2025-10-26 17:16:15 +0530

    Update session.h

--- session.h
+++ session.h
@@ -1,7 +1,19 @@
 #ifndef SESSION_H
 #define SESSION_H
 
-void saveSession(char *msg);
-void showSession();
+typedef struct Node {
+    int slot;
+    char vehicle[20];
+    char user[20];
+    long inTime;
+    struct Node *next;
+} Node;
+
+extern Node *head;
+
+void addSession(int slot, char *vehicle, char *user, long inTime);
+void endSession(int slot);
+void showSessions();
+Node* findSessionBySlot(int slot); // 🔍 new DSA-based search function
 
 #endif
